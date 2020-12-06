3084bf88daa2d2eb88e586369de8a7607dd9b720
Lekosystem <dj.leko.system@gmail.com>
2020-12-06 13:49:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "token": "TOKEN", // https://discordapp.com/developers/applications/ID/bot
-  "mongodbUrl": "MONGODB_URL", // Mongodb connection url.
-  "id": "CLIENT_ID", // https://discordapp.com/developers/applications/ID/information
-  "clientSecret": "CLIENT_SECRET", // https://discordapp.com/developers/applications/ID/information
+  "token": "Nzg0Nzk0OTE4OTkyMjE2MDY0.X8ufOQ.Q0FDxoDid06HV9szxq46Sq-edJ8", // https://discordapp.com/developers/applications/ID/bot
+  "mongodbUrl": "mongodb+srv://Lekosystem:q3sUxXSgtLGPJsSQ@cluster0.sd4fw.mongodb.net/discord?authSource=admin&replicaSet=atlas-t10rwg-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", // Mongodb connection url.
+  "id": "784794918992216064", // https://discordapp.com/developers/applications/ID/information
+  "clientSecret": "qu1xaBZreMNL2u70LH6NM_I9qifRHFd3", // https://discordapp.com/developers/applications/ID/information
   "domain": "http://localhost",
   "port": 8080
 };
